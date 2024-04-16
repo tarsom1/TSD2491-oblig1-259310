eea20d8d914b382a6a0e427449ec91172bc51662
tarsom1 <166919941+tarsom1@users.noreply.github.com>
2024-04-16 14:59:42 +0200

    Initial commit

---
+++ TSD2491-oblig1-259310/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TSD2491_oblig1_259310.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

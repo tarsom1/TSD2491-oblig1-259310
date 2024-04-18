2fb21cc6e7ded8eb13a4da5c88ca2e6e9a81f3c3
tarsom1 <166919941+tarsom1@users.noreply.github.com>
2024-04-18 14:57:21 +0200

    commit 4
    la til kode for å kjøre spillet på nettsiden + skjermbilde av spillet

--- TSD2491-oblig1-259310/Controllers/HomeController.cs
+++ TSD2491-oblig1-259310/Controllers/HomeController.cs
@@ -6,27 +6,20 @@ namespace TSD2491_oblig1_259310.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		private readonly static MatchinggameModel _matchingGameModels = new MatchinggameModel();
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
+
+
+        public HomeController()
+        {
+			
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			return View(_matchingGameModels);
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+       
 	}
 }
--- TSD2491-oblig1-259310/Models/MatchinggameModel.cs
+++ TSD2491-oblig1-259310/Models/MatchinggameModel.cs
@@ -6,8 +6,10 @@ namespace TSD2491_oblig1_259310.Models
 {
     public class MatchinggameModel
     {
-        
-        
+        public int MatchesFound { get; private set; }
+        public List<string> ShuffledEmoji { get; private set; }
+        private string LastAnimalFound = string.Empty;
+        private string LastDescription = string.Empty;
 
         private List<string> animalEmoji = new List<string>()
         {
@@ -23,5 +25,27 @@ namespace TSD2491_oblig1_259310.Models
             "🚐", "🚐", "🚒", "🚒"
         };
 
+        private static Random random = new Random();
+
+        public MatchinggameModel()
+        {
+            SetUpGame();
+        }
+
+        private List<string> PickRandomEmoji()
+        {
+            var emojiList = (random.Next(0, 2) == 0) ? animalEmoji : carEmoji;
+            return new List<string>(emojiList.OrderBy(e => random.Next()));
+        }
+
+        private void SetUpGame()
+        {
+            ShuffledEmoji = PickRandomEmoji();
+            MatchesFound = 0;
+            LastAnimalFound = string.Empty;
+            LastDescription = string.Empty;
+        }
+
+       
     }
 }
